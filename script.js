c12e37560cc6d5ee8cb7cce7e9b030c595496171
ali osmar <ali.osmar.chehade@gmail.com>
2025-03-15 15:08:11 -0300

    adicionando javaScript

---
+++ script.js
@@ -0,0 +1,20 @@
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+
+var prevScrollpos = window.scrollY;
+window.onscroll=function () {
+    var currentScrollpos = window.scrollY;
+    if(prevScrollpos > currentScrollpos){
+        document.getElementById("navbar").style.top = "0";
+    }
+    else{
+        document.getElementById("navbar").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollpos;
+}
