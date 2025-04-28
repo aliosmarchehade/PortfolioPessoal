903f20318dc0434c612971301d1d9f895db69e4c
ali osmar <ali.osmar.chehade@gmail.com>
2025-04-28 17:54:58 -0300

    Estilizando idiomas

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-    function hamburg(){
-        const navbar = document.querySelector(".dropdown")
-        navbar.style.transform = "translateY(0px)"
-    }
-    function cancel(){
-        const navbar = document.querySelector(".dropdown")
-        navbar.style.transform = "translateY(-500px)"
-    }
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
 
 var prevScrollpos = window.scrollY;
 window.onscroll=function () {
@@ -81,63 +81,5 @@ console.log(chave);
 if(Linguagem[chave]){
     elemento.setAttribute("placeholder", Linguagem[chave]);
 }
-
-})};
-
-
-    function closeMenu() {
-        cancel(); 
-        
-        setTimeout(() => {
-            document.body.style.overflow = "auto"; 
-        }, 300);
-    }
-    function enviarMensagem(event) {
-    event.preventDefault();
-    
-    Swal.fire({
-        title: 'Mensagem enviada!',
-        text: 'Obrigado por entrar em contato. Responderei em breve!',
-        icon: 'success',
-        confirmButtonColor: '#6d4300'
-    });
-
-    event.target.reset();
-}
-
-let idiomarAtual = "en";
-
-function alterarIdioma(idioma){
-    fetch(`json/${idioma}.json`)
-    .then(data => data.json())
-    .then(data =>{
-        traduzirPagina(data);
-    })
-}
-function traduzirPagina(Linguagem){
-    document.querySelectorAll("[data-i18n]").forEach(elemento=>{
-        const chave = elemento.getAttribute("data-i18n");
-        if(Linguagem[chave]){
-            elemento.textContent = Linguagem[chave];
-        }
-    })
-
-document.querySelectorAll("[data-i18n-alt]").forEach(elemento=>{
-    console.log(elemento);
-    const chave = elemento.getAttribute("data-i18n-alt");
-    console.log(chave);
-    if(Linguagem[chave]){
-        elemento.setAttribute("alt", Linguagem[chave]);
-    }
-
-})
-
-document.querySelectorAll("[data-i18n-placeholder]").forEach(elemento=>{
-    console.log(elemento);
-    const chave = elemento.getAttribute("data-i18n-placeholder");
-    console.log(chave);
-    if(Linguagem[chave]){
-        elemento.setAttribute("placeholder", Linguagem[chave]);
-    }
 
 })};
